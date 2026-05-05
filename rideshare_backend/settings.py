09e50dcf03054b0ce2c43c2753df2ca1af848d65
ANAND S BOSCO <athuljoyal123@gmail.com>
2026-05-06 01:04:13 +0530

    fix mysql host railway

--- rideshare_backend/settings.py
+++ rideshare_backend/settings.py
@@ -74,18 +74,16 @@ WSGI_APPLICATION = 'rideshare_backend.wsgi.application'
 ASGI_APPLICATION = 'rideshare_backend.asgi.application'
 
 DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQLDATABASE', 'rideshare_db'),
-        'USER': os.getenv('MYSQLUSER', 'rideshare_user'),
-        'PASSWORD': os.getenv('MYSQLPASSWORD', 'ridesharepassword'),
-        'HOST': os.getenv('MYSQLHOST', 'db'),
+        'NAME': os.getenv('MYSQLDATABASE'),
+        'USER': os.getenv('MYSQLUSER'),
+        'PASSWORD': os.getenv('MYSQLPASSWORD'),
+        'HOST': os.getenv('MYSQLHOST'),
         'PORT': os.getenv('MYSQLPORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
     }
+
 }
 
 LOGIN_URL = 'login'
